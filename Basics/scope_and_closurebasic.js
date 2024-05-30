df3736f1eb288604042727db02f71d4ee46be7c2
Peri Reddy <peri.butukuri@gmail.com>
2024-05-31 01:09:18 +0530

    Basic Closure

---
+++ Basics/scope_and_closurebasic.js
@@ -0,0 +1,65 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+one()
+
+// we have call two() explicitly if we just call one() without two() then two() gets defined
+//but does not executed.
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+//In JS variables can even hold functions
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
+
+// we cannot access addTwo() before intializing
